03c985abb3e4592fa6af5e3859b6184e72a83910
emailanuj <emai.anujdubey@gmail.com>
2019-12-25 19:41:08 +0530

    Styling Django

--- djangonewsapp/NewsApp/forms.py
+++ djangonewsapp/NewsApp/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 
 class RegisterUserForm(forms.Form):
-    username=forms.CharField(max_length=50)
-    password=forms.CharField(max_length=50)
-    email=forms.CharField(max_length=50)
-    phone=forms.CharField(max_length=50)
+    username=forms.CharField(max_length=50,
+    widget=forms.TextInput(attrs={
+        'class':'form-control',
+        'placeholder':'Username'
+    }))
+    password=forms.CharField(max_length=50, widget=forms.TextInput(attrs={
+        'class':'form-control',
+        'placeholder':'Password',
+        'type':'password'
+    }))
+    email=forms.CharField(max_length=50, widget=forms.TextInput(attrs={
+        'class':'form-control',
+        'placeholder':'Email',
+        'type':'email'
+    }))
+    phone=forms.CharField(max_length=50, widget=forms.TextInput(attrs={
+        'class':'form-control',
+        'placeholder':'Phone'
+    }))
